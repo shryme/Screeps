70ca7474902d4626fc3c1906d8a03498569a4cc8
Shryme <sebpaquets@gmail.com>
2015-03-02 18:00:24 -0500

    Healers now return to flag1 if they have no Game.HEAL part working

--- healer.js
+++ healer.js
@@ -2,6 +2,9 @@ module.exports = function (creep) {
 
   var target = creep.pos.findClosest(Game.MY_CREEPS, {filter: function(object) {return object.hits < object.hitsMax && object.id !== creep.id;} });
   if (target) {
+
+    if (creep.getActiveBodyparts(Game.HEAL) == 0 && Game.flags.Flag1 !== undefined)
+      target = Game.flags.Flag1;
     //creep.say('HEAL');
     creep.moveTo(target);
     creep.heal(target);
