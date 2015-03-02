ea3aba221a3de5a5d8b001fbb378acc3a25a7dfa
Shryme <sebpaquets@gmail.com>
2015-03-02 18:02:51 -0500

    corrected mistake

--- lazy_harvester_factory.js
+++ lazy_harvester_factory.js
@@ -82,7 +82,7 @@ module.exports = {
     else {
       if (Game.spawns.Spawn1.energy > 800 + 160 + 100 * Game.spawns.Spawn1.memory.path.length - 2)
         return true;
-      var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 8);
+      var targets = Game.spawns.Spawn1.pos.findInRange(Game.HOSTILE_CREEPS, 8);
       if (targets)
         return true;
     }
