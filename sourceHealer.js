845c9e0cc8c666d692ef4b4c21cfbba99a4bc01e
Shryme <sebpaquets@gmail.com>
2015-03-03 23:53:06 -0500

    Long live to copy pasta

--- sourceHealer.js
+++ sourceHealer.js
@@ -1,5 +1,5 @@
 module.exports = function (creep) {
-  if (creep.pos.x !== creep.memory.toGo.x && creep.pos.y !== creep.memory.toGo) {
+  if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
     creep.moveTo(creep.memory.toGo);
   }
   else {
