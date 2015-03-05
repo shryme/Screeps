bc81f00c8ce707aa7d8c23f52c430ec632b0e709
shryme <sebpaquets@gmail.com>
2015-03-04 19:26:12 -0500

    Trying to automate source keeper destruction

--- sourceDestroyer.js
+++ sourceDestroyer.js
@@ -1,13 +1,23 @@
 module.exports = function (creep) {
-  if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
-    creep.moveTo(creep.memory.toGo);
-  }
-  else {
-    var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
-    if(targets.length > 0) {
-      creep.rangedAttack(targets[0]);
-    }
-  }
+
+	var sourceDistance = creep.pos.getRangeTo(Game.spawns.Spawn1.memory.sourceKeeperPos);
+	var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
+	if (targets > 0) {
+		creep.rangedAttack(targets[0]);
+	}
+	if (range > 3) {
+		creep.moveTo(Game.spawns.Spawn1.memory.sourceKeeperPos);
+	}
+
+	// if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
+	// 	creep.moveTo(creep.memory.toGo);
+	// }
+	// else {
+	// 	var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
+	// 	if(targets.length > 0) {
+	// 		creep.rangedAttack(targets[0]);
+	// 	}
+	// }
 }
 
 //spawn at 380
