d48c7bdbe5eec8533418d43fe7ebb5e300073434
shryme <sebpaquets@gmail.com>
2015-03-04 20:06:55 -0500

    Trying to automate them to follow the current source_destroyer

--- sourceHealer.js
+++ sourceHealer.js
@@ -1,10 +1,22 @@
 module.exports = function (creep) {
-  if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
-    creep.moveTo(creep.memory.toGo);
-  }
-  else {
-    var tm = creep.pos.findClosest(Game.MY_CREEPS, {filter: function(object) {return object.memory.role === 'source_destroyer'} });
-    if (tm)
-      creep.heal(tm);
-  }
+
+	var target = creep.pos.findClosest(Game.MY_CREEPS, {filter: function(object) {return object.hits < object.hitsMax && object.id !== creep.id && object.memory.role === 'source_destroyer';} });
+
+	if (target === undefined)
+		target = creep.pos.findClosest(Game.MY_CREEPS, {filter: function(object) {return object.memory.role === 'source_destroyer'} });
+
+	if (target) {
+		creep.moveTo(target);
+		creep.heal(target);
+	}
+
+	// if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
+	// 	creep.moveTo(creep.memory.toGo);
+	// }
+	// else {
+	// 	var tm = creep.pos.findClosest(Game.MY_CREEPS, {filter: function(object) {return object.memory.role === 'source_destroyer'} });
+	// 	if (tm)
+	// 		creep.heal(tm);
+	// }
+
 }
