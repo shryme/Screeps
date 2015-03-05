8c59247a8fdda8dfa668a848d3e75406b13f52f5
shryme <sebpaquets@gmail.com>
2015-03-04 22:33:07 -0500

    changed detection of hp

--- sourceDestroyer.js
+++ sourceDestroyer.js
@@ -3,7 +3,7 @@ module.exports = function (creep) {
 	var sourceDistance = creep.pos.getRangeTo(Game.spawns.Spawn1.memory.sourceKeeperPos);
 	var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
 	if (targets.length > 0) {
-		if (targets[0].getActiveBodyparts() > 1) {
+		if (targets[0].hits > 100) {
 			creep.rangedAttack(targets[0]);
 			Game.spawns.Spawn1.memory.source_harvester_bypass = false;
 		}
