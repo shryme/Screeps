0f54e73b775c99b44e18eb7c92039707f1065ce8
shryme <sebpaquets@gmail.com>
2015-03-04 20:06:30 -0500

    removed comments

--- sourceDestroyer.js
+++ sourceDestroyer.js
@@ -9,15 +9,6 @@ module.exports = function (creep) {
 		creep.moveTo(Game.spawns.Spawn1.memory.sourceKeeperPos);
 	}
 
-	// if (creep.pos.x !== creep.memory.toGo.x || creep.pos.y !== creep.memory.toGo.y) {
-	// 	creep.moveTo(creep.memory.toGo);
-	// }
-	// else {
-	// 	var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
-	// 	if(targets.length > 0) {
-	// 		creep.rangedAttack(targets[0]);
-	// 	}
-	// }
 }
 
 //spawn at 380
