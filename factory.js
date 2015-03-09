dec47bf3c24ddf12f5c0b9dbecff888326981ccb
shryme <sebpaquets@gmail.com>
2015-03-09 18:55:47 -0400

    started trying to find where is the problem

--- factory.js
+++ factory.js
@@ -11,187 +11,180 @@ prices.heal = 200;
 prices.tough = 20;
 
 
-function getCost(arr) {
-	var total = 0;
-	for (var i = 0; i < arr.length; i++) {
-		total += prices[arr[i]];
-	}
+// function getCost(arr) {
+// 	var total = 0;
+// 	for (var i = 0; i < arr.length; i++) {
+// 		total += prices[arr[i]];
+// 	}
 
-	return total;
-}
+// 	return total;
+// }
 
-function canSpawnUnit(arr) {
-	var unit = getCost(arr);
-	var harvester = getCost([Game.WORK, Game.WORK, Game.WORK, Game.CARRY, Game.MOVE]);
-	var bus = getCost([Game.CARRY, Game.MOVE]);
-	var allBus = bus * (Game.spawns.Spawn1.memory.path.length - 2);
+// function canSpawnUnit(arr) {
+// 	var unit = getCost(arr);
+// 	var harvester = getCost([Game.WORK, Game.WORK, Game.WORK, Game.CARRY, Game.MOVE]);
+// 	var bus = getCost([Game.CARRY, Game.MOVE]);
+// 	var allBus = bus * (Game.spawns.Spawn1.memory.path.length - 2);
 
-	if (Game.spawns.Spawn1.energy > unit + harvester + allBus) {
-		return true;
-	}
-	return false
-}
+// 	if (Game.spawns.Spawn1.energy > unit + harvester + allBus) {
+// 		return true;
+// 	}
+// 	return false
+// }
 
-function spawnWeakGuard() {
-	var modules = [Game.TOUGH, Game.MOVE, Game.ATTACK, Game.MOVE, Game.ATTACK, Game.RANGED_ATTACK];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
-}
+// function spawnWeakGuard() {
+// 	var modules = [Game.TOUGH, Game.MOVE, Game.ATTACK, Game.MOVE, Game.ATTACK, Game.RANGED_ATTACK];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
+// }
 
-function spawnWeakBuilder() {
-	var modules = [Game.CARRY, Game.CARRY, Game.WORK, Game.WORK, Game.MOVE];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'builder'});
-}
+// function spawnWeakBuilder() {
+// 	var modules = [Game.CARRY, Game.CARRY, Game.WORK, Game.WORK, Game.MOVE];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'builder'});
+// }
 
-function spawnWeakHealer(targetRole) {
-	var modules = [Game.HEAL, Game.HEAL, Game.HEAL, Game.MOVE, Game.MOVE];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'healer', targetRole: targetRole});
-}
+// function spawnWeakHealer(targetRole) {
+// 	var modules = [Game.HEAL, Game.HEAL, Game.HEAL, Game.MOVE, Game.MOVE];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'healer', targetRole: targetRole});
+// }
 
-function spawnExtensionGuard(pieces) {
-	if (canSpawnUnit(pieces))
-		Game.spawns.Spawn1.createCreep(pieces, undefined, {role: 'guard'});
-}
+// function spawnExtensionGuard(pieces) {
+// 	if (canSpawnUnit(pieces))
+// 		Game.spawns.Spawn1.createCreep(pieces, undefined, {role: 'guard'});
+// }
 
-function spawnToughGuard() {
-	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.MOVE, Game.MOVE, Game.ATTACK, Game.ATTACK, Game.ATTACK];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
-}
+// function spawnToughGuard() {
+// 	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.MOVE, Game.MOVE, Game.ATTACK, Game.ATTACK, Game.ATTACK];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
+// }
 
-function spawnMediumGuard() {
-	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.ATTACK, Game.ATTACK, Game.ATTACK, Game.MOVE, Game.ATTACK];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
-}
+// function spawnMediumGuard() {
+// 	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.ATTACK, Game.ATTACK, Game.ATTACK, Game.MOVE, Game.ATTACK];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'guard'});
+// }
 
-var sourceDestroyerPosition = {x: 45, y: 26};
+// var sourceDestroyerPosition = {x: 45, y: 26};
 
-var listSourceHealer = new Array();
-listSourceHealer.push({x: 45, y: 25});
-listSourceHealer.push({x: 46, y: 25});
+// var listSourceHealer = new Array();
+// listSourceHealer.push({x: 45, y: 25});
+// listSourceHealer.push({x: 46, y: 25});
 
-function getListOfOpenPos(list) {
-	var newList = new Array();
+// function getListOfOpenPos(list) {
+// 	var newList = new Array();
 
-	for (var i = 0; i < listSourceHealer.length; i++) {
+// 	for (var i = 0; i < listSourceHealer.length; i++) {
 
-		var x = listSourceHealer[i].x;
-		var y = listSourceHealer[i].y;
+// 		var x = listSourceHealer[i].x;
+// 		var y = listSourceHealer[i].y;
 
-		var isUsed = false;
-		for (var j = 0; j < list.length; j++) {
-			if (list[j].toGo.x === x && list[j].toGo.y === y) {
-				isUsed = true;
-				break;
-			}
-		}
+// 		var isUsed = false;
+// 		for (var j = 0; j < list.length; j++) {
+// 			if (list[j].toGo.x === x && list[j].toGo.y === y) {
+// 				isUsed = true;
+// 				break;
+// 			}
+// 		}
 
-		if (!isUsed)
-			newList.push(listSourceHealer[i]);
-	}
+// 		if (!isUsed)
+// 			newList.push(listSourceHealer[i]);
+// 	}
 
-	return newList;
-}
+// 	return newList;
+// }
 
-function spawnSourceHealer(list, isTemp) {
-	var modules = [Game.HEAL, Game.HEAL, Game.HEAL, Game.HEAL, Game.MOVE];
+// function spawnSourceHealer(list, isTemp) {
+// 	var modules = [Game.HEAL, Game.HEAL, Game.HEAL, Game.HEAL, Game.MOVE];
 
-	var listPos = getListOfOpenPos(list);
+// 	var listPos = getListOfOpenPos(list);
 
-	if (listPos.length === 0 && isTemp === true)
-		listPos.push({x: 44, y: 25});
+// 	if (listPos.length === 0 && isTemp === true)
+// 		listPos.push({x: 44, y: 25});
 
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_healer', toGo: listPos[0]});
-}
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_healer', toGo: listPos[0]});
+// }
 
-function findClosestSourceKeeperSpot(from) {
-	//Game.spawns.Spawn1.pos.findClosest(Game.HOSTILE_STRUCTURES).ticksToSpawn;
+// function findClosestSourceKeeperSpot(from) {
+// 	//Game.spawns.Spawn1.pos.findClosest(Game.HOSTILE_STRUCTURES).ticksToSpawn;
 
-	if (Game.spawns.Spawn1.memory.sourceKeeperPos === undefined) {
-		var lair = from.pos.findClosest(Game.HOSTILE_STRUCTURES);
-		var sourceKeeper = lair.pos.findClosest(Game.HOSTILE_CREEPS);
-		sourceKeeper.pos.x = sourceKeeper.pos.x + 1;
-		Game.spawns.Spawn1.memory.sourceKeeperPos = sourceKeeper.pos;
-	}
+// 	if (Game.spawns.Spawn1.memory.sourceKeeperPos === undefined) {
+// 		var lair = from.pos.findClosest(Game.HOSTILE_STRUCTURES);
+// 		var sourceKeeper = lair.pos.findClosest(Game.HOSTILE_CREEPS);
+// 		sourceKeeper.pos.x = sourceKeeper.pos.x + 1;
+// 		Game.spawns.Spawn1.memory.sourceKeeperPos = sourceKeeper.pos;
+// 	}
 
-	return Game.spawns.Spawn1.memory.sourceKeeperPos;
-
-
-}
+// 	return Game.spawns.Spawn1.memory.sourceKeeperPos;
 
 
-function spawnSourceDestroyer() {
-	var keeperPos = findClosestSourceKeeperSpot(Game.spawns.Spawn1);
-	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH,Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.MOVE];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_destroyer', targetPos: keeperPos});
-}
+// }
 
-function spawnSourceHarvester() {
-	var modules = [Game.WORK, Game.CARRY, Game.MOVE, Game.CARRY, Game.MOVE];
-	if (canSpawnUnit(modules))
-		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_harvester'});
-}
+
+// function spawnSourceDestroyer() {
+// 	var keeperPos = findClosestSourceKeeperSpot(Game.spawns.Spawn1);
+// 	var modules = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH,Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.MOVE];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_destroyer', targetPos: keeperPos});
+// }
+
+// function spawnSourceHarvester() {
+// 	var modules = [Game.WORK, Game.CARRY, Game.MOVE, Game.CARRY, Game.MOVE];
+// 	if (canSpawnUnit(modules))
+// 		Game.spawns.Spawn1.createCreep(modules, undefined, {role: 'source_harvester'});
+// }
 
 module.exports = {
 
 	createRobotz: function(data) {
 
-		if (lazyHarvesterFactory.isDone(data.lazy_harvesterNumbers, data.busNumbers, data.listBus, data.listHarvester)) {
+		// if (lazyHarvesterFactory.isDone(data.lazy_harvesterNumbers, data.busNumbers, data.listBus, data.listHarvester)) {
 
-			/*var targets = Game.spawns.Spawn1.pos.findInRange(Game.HOSTILE_CREEPS, 8);
-			if (targets.length > 0) {
+		// 	/*var targets = Game.spawns.Spawn1.pos.findInRange(Game.HOSTILE_CREEPS, 8);
+		// 	if (targets.length > 0) {
 
-			}*/
+		// 	}*/
 
-			/*if (data.guardNumbers >= 8 && Game.spawns.Spawn1) {
-				Game.spawns.Spawn1.createCreep([Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.MOVE, Game.RANGED_ATTACK], undefined, {role: 'guard'});
-			}*/
-
-			//1950 source keeper die, revive 2100
-
-			var extNumbers = Game.spawns.Spawn1.room.find(Game.MY_STRUCTURES, {filter: function(object) {return object.structureType === Game.STRUCTURE_EXTENSION && object.energy === object.energyCapacity} }).length;
-
-			if (data.guardNumbers < 2) {
-				spawnWeakGuard();
-			}
-			else if (data.sourceDestroyerNumbers < 1 || (data.destroyerRefresh && data.sourceDestroyerNumbers < 2)) {
-				spawnSourceDestroyer();
-			}
-			else if (data.sourceHealerNumbers < 2 || (data.destroyerRefresh && data.sourceHealerNumbers < 3)) {
-				spawnSourceHealer(data.listSourceHealer, data.destroyerRefresh);
-			}
-			//else if (Game.spawns.Spawn1.room.find(Game.CONSTRUCTION_SITES).length > 0 && data.builderNumbers === 0 || data.builderNumbers === 0 && extNumbers > 0) {
-			//  spawnWeakBuilder();
-			//}
-			else if (data.healerNumbers < 2 || data.needHealer === true) {
-				spawnWeakHealer();
-			}
-			else if (data.sourceHarvesterNumbers < 3) {
-				spawnSourceHarvester();
-			}
-			// else if (data.healerNumbers < 4) {
-			// 	spawnWeakHealer('source_harvester');
-			// }
-			else if (extNumbers > 0) {
-				var pieces = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.ATTACK, Game.ATTACK, Game.MOVE, Game.MOVE, Game.MOVE];
-				for (var i = 0; i < extNumbers; i++)
-					pieces.push(Game.ATTACK);
-				spawnExtensionGuard(pieces);
-			}
-			else if (data.healerNumbers >= 2 && data.guardNumbers < 10 && Game.spawns.Spawn1) {
-				spawnToughGuard();
-			}
-			else if (data.guardNumbers >= 10 && Game.spawns.Spawn1) {
-				spawnMediumGuard();
-			}
+		// 	/*if (data.guardNumbers >= 8 && Game.spawns.Spawn1) {
+		// 		Game.spawns.Spawn1.createCreep([Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.MOVE, Game.RANGED_ATTACK], undefined, {role: 'guard'});
+		// 	}*/
 
 
-		}
+		// 	var extNumbers = Game.spawns.Spawn1.room.find(Game.MY_STRUCTURES, {filter: function(object) {return object.structureType === Game.STRUCTURE_EXTENSION && object.energy === object.energyCapacity} }).length;
+
+		// 	if (data.guardNumbers < 2) {
+		// 		spawnWeakGuard();
+		// 	}
+		// 	else if (data.sourceDestroyerNumbers < 1 || (data.destroyerRefresh && data.sourceDestroyerNumbers < 2)) {
+		// 		spawnSourceDestroyer();
+		// 	}
+		// 	else if (data.sourceHealerNumbers < 2 || (data.destroyerRefresh && data.sourceHealerNumbers < 3)) {
+		// 		spawnSourceHealer(data.listSourceHealer, data.destroyerRefresh);
+		// 	}
+		// 	else if (data.healerNumbers < 2 || data.needHealer === true) {
+		// 		spawnWeakHealer();
+		// 	}
+		// 	else if (data.sourceHarvesterNumbers < 3) {
+		// 		spawnSourceHarvester();
+		// 	}
+		// 	else if (extNumbers > 0) {
+		// 		var pieces = [Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.TOUGH, Game.ATTACK, Game.ATTACK, Game.MOVE, Game.MOVE, Game.MOVE];
+		// 		for (var i = 0; i < extNumbers; i++)
+		// 			pieces.push(Game.ATTACK);
+		// 		spawnExtensionGuard(pieces);
+		// 	}
+		// 	else if (data.healerNumbers >= 2 && data.guardNumbers < 10 && Game.spawns.Spawn1) {
+		// 		spawnToughGuard();
+		// 	}
+		// 	else if (data.guardNumbers >= 10 && Game.spawns.Spawn1) {
+		// 		spawnMediumGuard();
+		// 	}
+
+
+		// }
 
 	}
 
