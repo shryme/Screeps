291eb93bc8ede438cc294ec91580083a2abeee99
shryme <sebpaquets@gmail.com>
2015-03-10 20:25:06 -0400

    Oups

--- sourceDestroyer.js
+++ sourceDestroyer.js
@@ -1,9 +1,10 @@
 module.exports = function (creep) {
 
-	var isMoving = sourceDistance >= 0 ? true : false;
-
 	//Destroy the source keeper and tell everyone it's done once it is
 	var sourceDistance = creep.pos.getRangeTo(Game.spawns.Spawn1.memory.sourceKeeperPos);
+
+	var isMoving = sourceDistance >= 0 ? true : false;
+	
 	var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
 	if (targets.length > 0) {
 		if (targets[0].hits > 100) {
